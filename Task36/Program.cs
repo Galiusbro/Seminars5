37977a9804527f172027f709c885a8edd537a38e
Galprim <galprimulus@gmail.com>
2022-04-30 16:29:15 +0300

    add Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArray(int[] fillArray)
+{
+    int length = fillArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{fillArray[i] = new Random().Next(-100, 101)} ");
+    }
+    Console.WriteLine();
+}
+
+void EvenIndex(int[] evenArray)
+{
+    int length = evenArray.Length;
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if(i % 2 != 0) sum += evenArray[i];
+    }
+    Console.Write($"Сумма всех элементов с четным индексом: {sum}");
+}
+
+int[] array = new int[7];
+
+FillArray(array);
+EvenIndex(array);
