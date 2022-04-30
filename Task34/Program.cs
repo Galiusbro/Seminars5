d8b49a2c04b008b22dc4c0768a40f5f681fbf397
Galprim <galprimulus@gmail.com>
2022-04-30 16:00:31 +0300

    add Task 34

---
+++ Task34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void FillArray(int[] fillArray)
+{
+    int length = fillArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{fillArray[i] = new Random().Next(100, 1000)} ");
+    }
+    Console.WriteLine();
+}
+
+void ModTwo(int[] modArray)
+{
+    int length = modArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(modArray[i] % 2 == 0) Console.Write($"{modArray[i]} ");
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+ModTwo(array);
